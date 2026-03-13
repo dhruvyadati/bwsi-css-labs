7f0c6eb5c23a66d898f9bcff67c64e7c8b14aa81
Dhruv Yadati <86134012+dhruvyadati@users.noreply.github.com>
2026-03-12 22:47:48 -0700

    Update lab_1a.py
    Adding another line for introduction.

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Dhruv Yadati" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print(f"Check out the lab tasks!")
 
 if __name__ == "__main__":
     main()
